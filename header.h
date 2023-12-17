21153aedc67e00a010cde7b8ab1116af8052407b
Yaroslav <yarik.peresypkin4070@gmail.com>
2023-12-18 00:10:54 +0300

    Baza!

---
+++ header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER
+#define HEADER
+//Тестовые коммиты 2
+//Я устал
+
+// Метод для сложения
+double Slozenie(double a, double b);
+
+// Метод для вычитания
+double Vichitanie(double a, double b);
+
+// Метод для умножения
+double Ymnozhenie(double a, double b);
+
+// Метод для деления
+double Delenie(double a, double b);
+
+#endif // HEADER
+
