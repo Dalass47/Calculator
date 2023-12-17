c9f8c6942839b60d0c51623ff373533144d61f13
Yaroslav <yarik.peresypkin4070@gmail.com>
2023-12-18 01:53:15 +0300

    Add slozenie

---
+++ func.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "header.h"
+#include <cmath>
+#include <clocale>
+using namespace std;
+
+
+void Arifmetika::slozenie(){
+
+       cout << "¬ведите первую цифру" << endl;
+       cin >> a;
+       cout << "¬ведите вторую цифру" << endl;
+       cin >> b;
+       result = a + b;
+       cout << "—умма двух чисел равна  = " << result << endl;
+}
--- header.h
+++ header.h
@@ -2,18 +2,11 @@
 #define HEADER
 //Тестовые коммиты 2
 //Я устал
-
-// Метод для сложения
-double Slozenie(double a, double b);
-
-// Метод для вычитания
-double Vichitanie(double a, double b);
-
-// Метод для умножения
-double Ymnozhenie(double a, double b);
-
-// Метод для деления
-double Delenie(double a, double b);
+class Arifmetika{
+public:
+    double a,b,result;
+    void slozenie();
+};
 
 #endif // HEADER
 
